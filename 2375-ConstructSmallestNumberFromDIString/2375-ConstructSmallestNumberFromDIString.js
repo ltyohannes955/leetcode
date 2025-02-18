b784dc31c269b1b32532c2944290d631d129651b
Leul Yohannes <106162490+ltyohannes955@users.noreply.github.com>
2025-02-18 07:47:13 -0800

    Create file

---
+++ 2375-ConstructSmallestNumberFromDIString/2375-ConstructSmallestNumberFromDIString.js
@@ -0,0 +1,17 @@
+var smallestNumber = function(pattern) {
+    let n = pattern.length;
+    let result = "";
+    let stack = [];
+
+    for (let i = 0; i <= n; i++) {
+        stack.push(i + 1);
+
+        if (i == n || pattern[i] === 'I') {
+            while (stack.length > 0) {
+                result += stack.pop();
+            }
+        }
+    }
+
+    return result;
+};
